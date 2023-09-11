1334473b7e17459d1dbd06a29d551c428de6eb46
Alex Wellbelove <alexwellbelove@gmail.com>
2023-09-11 10:49:16 -0700

    Update lakefile to use Named Package

--- lakefile.lean
+++ lakefile.lean
@@ -13,14 +13,14 @@ def ffiSrc := "native.c"
 def ffiO   := "ffi.o"
 def ffiLib := "ffi"
 
-target ffi.o (pkg : Package) : FilePath := do
+target ffi.o (pkg : NPackage "Socket") : FilePath := do
   let oFile := pkg.buildDir / ffiO
   let srcJob ← inputFile <| pkg.dir / cDir / ffiSrc
   buildFileAfterDep oFile srcJob fun srcFile => do
     let flags := #["-I", (← getLeanIncludeDir).toString, "-fPIC"]
     compileO ffiSrc oFile srcFile flags
 
-extern_lib ffi (pkg : Package) := do
+extern_lib ffi (pkg : NPackage "Socket") := do
   let name := nameToStaticLib ffiLib
   let ffiO ← fetch <| pkg.target ``ffi.o
   buildStaticLib (pkg.buildDir / "lib" / name) #[ffiO]
